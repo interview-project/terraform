acf706e9308e8868c277ea2bb8d244c8399e451d
Ken Erwin <ken@devopslibrary.com>
2018-08-09 22:11:27 -0400

    removing security stuff shortly

---
+++ destroy_infrastructure.sh
@@ -0,0 +1,9 @@
+# Get AMI ID
+jenkins_ami=$(cat ../packer/jenkins-manifest.json | jq ".builds[0].artifact_id" -r | sed 's/.*://')
+nexus_ami=$(cat ../packer/nexus-manifest.json | jq ".builds[0].artifact_id" -r | sed 's/.*://')
+
+terraform destroy -var AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+                  -var AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+                  -var "management_ip=$MANAGEMENT_IP" \
+                  -var "jenkins_ami=$jenkins_ami" \
+                  -var "nexus_ami=$nexus_ami"
--- main.tf
+++ main.tf
@@ -15,37 +15,48 @@ resource "aws_security_group" "provisioning_example-lb" {
     name = "provisioning_example lb"
     description = "Security group for the load balancer"
     ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["${var.management_ip}/32"]
     }
-
     ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["${var.management_ip}/32"]
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["${var.management_ip}/32"]
+    }
+    ingress {
+      from_port = 8081
+      to_port = 8081
+      protocol = "tcp"
+      cidr_blocks = ["${var.management_ip}/32"]
+    }
+    ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["${var.management_ip}/32"]
     }
 
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
     }
 }
 /* Our root SSH key pair is created by the wrapper script. */
 
 resource "aws_key_pair" "root" {
     key_name = "root-key"
-    public_key = "${file("id_rsa_example.pub")}"
+    public_key = "${file("id_rsa.pub")}"
 }
 
 # Create a Jenkins master
 resource "aws_instance" "jenkins" {
   ami           = "${var.jenkins_ami}"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "${aws_key_pair.root.key_name}"
   vpc_security_group_ids = ["${aws_security_group.provisioning_example-lb.id}"]
   tags {
@@ -56,7 +67,7 @@ resource "aws_instance" "jenkins" {
 # Create a Nexus Server
 resource "aws_instance" "nexus" {
   ami           = "${var.nexus_ami}"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "${aws_key_pair.root.key_name}"
   vpc_security_group_ids = ["${aws_security_group.provisioning_example-lb.id}"]
   tags {
